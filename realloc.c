c4cab64ab695e1e91b3f80994dd7b14a22624bac
Sylvain Corsini <sylvain.corsini@epitech.eu>
2017-02-09 17:19:53 +0100

    Patching realloc.

--- realloc.c
+++ realloc.c
@@ -18,7 +18,7 @@ void *thread_realloc(void *ptr, size_t size)
   void *new_block;
 
   if (ptr == NULL)
-    return (malloc(size));
+    return (thread_malloc(size));
   if (valid_block(ptr) == 1)
     {
       block = get_block(ptr);
